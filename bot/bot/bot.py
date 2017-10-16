7ce87b6d9057b8c710a213b5e9f1aa113c1e2454
Andrey <andreytrue10@gmail.com>
2017-10-17 01:36:54 +0300

    Initial commit

--- bot/bot/bot.py
+++ bot/bot/bot.py
@@ -2,16 +2,50 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
-from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
-admin=309139274
+admin=-1
 chat_id=-1
+password="1010"
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Есть русский язык!")
+    bot.reply_to(message, "Привет! Ты что-то пишешь, я пересылаю!")
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Я тестовый бот и мало что могу. В любом случае здесь есть команды помимо /help и /start, но они пока известны только девелоперы(Андрею).")
+
+@bot.message_handler(commands=['status'])
+def send_welcome(message):
+    bot.reply_to(message, "Я онлайн!")
+
+@bot.message_handler(commands=['log'])
+def log_in(message):
+    str=message.text.split(" ")
+    global admin
+    if admin!=-1:
+        bot.reply_to(message, "Админ уже авторизован.")
+    else:
+        if len(str)==2:
+            if str[1]==password:
+                admin=message.chat.id
+                bot.reply_to(message, "Вы авторизованы!")
+            else:
+                bot.reply_to(message, "Неверный пароль.")
+        else:
+            bot.reply_to(message, "Недостаточно, или слишком много параметров.")
+
+@bot.message_handler(commands=['off'])
+def log_off(message):
+    global admin
+    if (message.chat.id==admin):
+        admin=-1
+        bot.reply_to(message, "Вы больше не админ")
+    else:
+        bot.reply_to(message, "Вы и так не авторизованы")
+        
 
 
 
@@ -22,22 +56,24 @@ def switch_chat_id(message):
         global chat_id
         chat_id=int(str[1])
         bot.reply_to(message, "Переход в чат №"+str[1])
-        bot.send_message(chat_id,"Оператор зашел в ваш чатт")
+        bot.send_message(chat_id,"Оператор зашел в ваш чат")
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): #
-    if (message.chat.id==admin):
-        if chat_id!=-1:
-            bot.send_message(chat_id,message.text)
+    if admin!=-1:    
+        if (message.chat.id==admin):
+            if chat_id!=-1:
+                bot.send_message(chat_id,message.text)
+            else:
+                bot.send_message(admin,"Вы не находитесь ни в одном чате")
         else:
-            bot.send_message(admin,"Вы не находитесь ни в одном чате")
+            text=message.text+"\n"+"Отправил: "+message.from_user.first_name+" "+message.from_user.last_name+"\n"+"/chat_"+str(message.chat.id)
+            bot.send_message(admin, text)
     else:
-        bot.reply_to(message, "Отправляю сообщение хозяину")
-        text=message.text+"\n"+"Отправил: "+message.from_user.first_name+" "+message.from_user.last_name+"\n"+"/chat_"+str(message.chat.id)
-        bot.send_message(admin, text)
+        bot.reply_to(message, "Админа нет онлайн")
 
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+     bot.polling(none_stop=True)
