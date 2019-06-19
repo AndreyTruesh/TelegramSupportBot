778172948c467e7a98eaea0f9f3916e8b5ee80be
AndreyTruesh <andreytrue10@gmail.com>
2019-06-19 15:31:27 +0300

    Update bot.py

--- bot/bot/bot.py
+++ bot/bot/bot.py
@@ -11,40 +11,40 @@ password="1010"
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Ты что-то пишешь, я пересылаю!")
+    bot.reply_to(message, "Yo, I will transfer your msg to someone")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Я тестовый бот и мало что могу. В любом случае здесь есть команды помимо /help и /start, но они пока известны только девелоперы(Андрею).")
+    bot.reply_to(message, "Hi! I'm test bot and can't do much :d")
 
 @bot.message_handler(commands=['status'])
 def send_welcome(message):
-    bot.reply_to(message, "Я онлайн!")
+    bot.reply_to(message, "I'm online")
 
 @bot.message_handler(commands=['log'])
 def log_in(message):
     str=message.text.split(" ")
     global admin
     if admin!=-1:
-        bot.reply_to(message, "Админ уже авторизован.")
+        bot.reply_to(message, "Admin is logged on")
     else:
         if len(str)==2:
             if str[1]==password:
                 admin=message.chat.id
-                bot.reply_to(message, "Вы авторизованы!")
+                bot.reply_to(message, "You are logged on.")
             else:
-                bot.reply_to(message, "Неверный пароль.")
+                bot.reply_to(message, "Incorrect pass")
         else:
-            bot.reply_to(message, "Недостаточно, или слишком много параметров.")
+            bot.reply_to(message, "Wrong command")
 
 @bot.message_handler(commands=['off'])
 def log_off(message):
     global admin
     if (message.chat.id==admin):
         admin=-1
-        bot.reply_to(message, "Вы больше не админ")
+        bot.reply_to(message, "You're no longer admin")
     else:
-        bot.reply_to(message, "Вы и так не авторизованы")
+        bot.reply_to(message, "Did not authorized yet")
         
 
 
@@ -55,8 +55,8 @@ def switch_chat_id(message):
         str=message.text.split("_")
         global chat_id
         chat_id=int(str[1])
-        bot.reply_to(message, "Переход в чат №"+str[1])
-        bot.send_message(chat_id,"Оператор зашел в ваш чат")
+        bot.reply_to(message, "Going to chat №"+str[1])
+        bot.send_message(chat_id,"Support guy is joined")
 
 
 @bot.message_handler(content_types=["text"])
@@ -66,12 +66,12 @@ def repeat_all_messages(message): #
             if chat_id!=-1:
                 bot.send_message(chat_id,message.text)
             else:
-                bot.send_message(admin,"Вы не находитесь ни в одном чате")
+                bot.send_message(admin,"Choose a chast before type msg")
         else:
             text=message.text+"\n"+"Отправил: "+message.from_user.first_name+" "+message.from_user.last_name+"\n"+"/chat_"+str(message.chat.id)
             bot.send_message(admin, text)
     else:
-        bot.reply_to(message, "Админа нет онлайн")
+        bot.reply_to(message, "Support-Gu is not online")
 
 
 
